3725c1bb266a9631f58556b3abe2bd7fc014cc20
Niels Mokkenstorm <nielsmokkenstorm@gmail.com>
2021-03-01 12:05:09 +0100

    add zsh dots

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# symlink dots
+ln -sf  $(pwd)/.vimrc ~/.vimrc
+ln -sf  $(pwd)/.zshrc ~/.zshrc
