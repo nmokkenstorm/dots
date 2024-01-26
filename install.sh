154ef2e0111ca34b29bd4650d1e461239b5c4e9d
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2024-01-26 16:11:12 +0100

    fix pwd

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+root=$(pwd)
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # symlink dots
-ln -sf  $(pwd)/.vimrc ~/.vimrc
-ln -sf  $(pwd)/.zshrc ~/.zshrc
+ln -sf  $root/.vimrc ~/.vimrc
+ln -sf  $root/.zshrc ~/.zshrc
+
+mkdir -p $root/.config
+mkdir -p $root/.config/alacritty
+
+ln -sf  $root/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 # global gitignore
 git config --global core.excludesfile ~/.globalgitignore
