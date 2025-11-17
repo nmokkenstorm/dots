9ecde31338974218054f658234bd194b59fe5958
Niels Mokkenstorm <niels@mokkenstorm.dev>
2025-11-17 23:41:28 +0100

    Update install script and configs: add neovim config, 1Password SSH agent config, fix homebrew paths in zshrc, add missing symlinks

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # symlink dots
-ln -sf  ~/.vimrc ~/.vimrc
-ln -sf  ~/.zshrc ~/.zshrc
+ln -sf "$root/.vimrc" ~/.vimrc
+ln -sf "$root/.zshrc" ~/.zshrc
 
-mkdir -p ~.config
+mkdir -p ~/.config
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
-ln -sf  $root/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf "$root/.config/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
+
+ln -sf "$root/.config/nvim" ~/.config/nvim
 
 # global gitignore
 git config --global core.excludesfile ~/.globalgitignore
